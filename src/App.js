4d08b565fb251140afc4fc68b76985aa2af69ce7
전병훈 <ifries8611@gmail.com>
2021-05-11 23:14:20 +0900

    propType을 통한 객체 varidate처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
     {
@@ -6,7 +7,8 @@ const foodILike = [
         name: "Kimchi",
         isFav: "like",
         image:
-            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+        rating: 5
     },
     {
         id:2,
@@ -14,45 +16,56 @@ const foodILike = [
         isFav: "like",
 
         image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+        rating: 4.2
     },
     {
         id:3,
         name: "Bibimbap",
         isFav: "dont like",
         image:
-            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+        rating: 3.5
     },
     {
         id:4,
         name: "Doncasu",
         isFav: "dont like",
         image:
-            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+        rating: 4
     },
     {
         id:5,
         name: "Kimbap",
         isFav: "like",
         image:
-            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+        rating: 4.4
     }
 ];
 
-function Food({name, isFav, image}) {
+function Food({name, isFav, image, rating}) {
     return (
         <div>
             <h1>I {isFav} {name}</h1>
+            <h4>{rating} / 5.0 </h4>
             <img src={image} alt={name}/>
         </div>
     )
+}
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
 }
 
 function App() {
   return (
     <div>
         {foodILike.map(dish => (
-            <Food key={dish.id} name={dish.name} image={dish.image}/>
+            <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>
         ))}
     </div>
   );

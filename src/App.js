44c2957a41fc49670e87c78078ba1fffa5c1e77d
전병훈 <ifries8611@gmail.com>
2021-05-11 13:21:16 +0900

    컴포넌트 정의 및 props사용 예제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Test from "./Test";
+
+function Food({fav, isFav}) {
+    return <h1>I {isFav} {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Test />
+      <Food fav="kimchi" isFav="like" />
+      <Food fav="bab" isFav="dont like" />
+      <Food fav="ham" isFav="like" />
+      <Food fav="ramen" isFav="like" />
+      <Food fav="book" isFav="dont like" />
     </div>
   );
 }
--- src/Test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ComponentTest() {
-    return <h3>custom component test</h3>;
-}
-
-export default ComponentTest;

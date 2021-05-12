7127388ba6178b4f4f53b4f0354a62bb15294b42
전병훈 <ifries8611@gmail.com>
2021-05-12 13:04:35 +0900

    YLS의 API를 통해서 영화정보를 가져오고 Movie 컴포넌트를 정의하여 영화제목 redner

--- src/App.js
+++ src/App.js
@@ -1,55 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
-    // class가 생성될 때 호
-    constructor(props) {
-        super(props);
-        console.log("constructor")
-    }
     state = {
-        count:0
-    };
+        isLoading: true,
+        movies: []
+    }
 
-    add = () => {
-        console.log("add");
-        this.setState(current => ({
-            count: current.count + 1
-        }));
-    };
-    minus = () => {
-        this.setState( (current) => ({
-           count: current.count - 1
-        }));
-    };
+    getMovies = async () => {
+        const {
+            data:{
+                data:{
+                    movies
+                }
+            }
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+
+        console.log(movies);
+        this.setState({movies, isLoading:false});
+    }
+
+    componentDidMount() {
+        this.getMovies();
+
+    }
+
     render() {
-        console.log("rendering");
+        const { isLoading, movies } = this.state;
         return (
             <div>
-                <h1>
-                    The number is {this.state.count}
-                </h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
+                {isLoading ? "Loading..." : movies.map(movie => {
+                    return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+                })}
             </div>
         );
     };
-
-    // 컴포넌트가 render후에 호출
-    componentDidMount() {
-        console.log("component rendered");
-    }
-
-    // props나 state가 변경되었을 때 호출
-    // state -> render -> update
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("just updated");
-    }
-
-    // 컴포넌트가 종료되었을때 호출
-    componentWillUnmount() {
-        console.log("component unmount");
-    }
 }
 
 export default App;

92aabaeff406ac81e5253d665ff4065ef56fc443
전병훈 <ifries8611@gmail.com>
2021-05-12 23:03:16 +0900

    영화목록 컴포넌트 처리, css처리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
@@ -11,15 +12,15 @@ class App extends React.Component {
 
     getMovies = async () => {
         const {
-            data:{
-                data:{
+            data: {
+                data: {
                     movies
                 }
             }
         } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
 
         console.log(movies);
-        this.setState({movies, isLoading:false});
+        this.setState({movies, isLoading: false});
     }
 
     componentDidMount() {
@@ -28,13 +29,30 @@ class App extends React.Component {
     }
 
     render() {
-        const { isLoading, movies } = this.state;
+        const {isLoading, movies} = this.state;
         return (
-            <div>
-                {isLoading ? "Loading..." : movies.map(movie => {
-                    return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
-                })}
-            </div>
+            <section className="container">
+                {
+                    isLoading ? (
+                        <div className="loader">
+                            <span className="loader__text">Loading...</span>
+                        </div>
+                    ) : (
+                        <div className="movies">
+                            {movies.map(movie => (
+                                <Movie
+                                    key={movie.id}
+                                    id={movie.id}
+                                    year={movie.year}
+                                    title={movie.title}
+                                    summary={movie.summary}
+                                    poster={movie.medium_cover_image}
+                                    genres={movie.genres}
+                                />
+                            ))}
+                        </div>
+                    )}
+            </section>
         );
     };
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
+function Movie({id, year, title, summary, poster, genres}) {
     return (
-        <div>
-            <h5>{title}</h5>
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h3 className="movies__title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres"> {genres.map((genre, index) =>
+                    <li key={index} className="genres__genre">{genre}</li>
+                )}</ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
         </div>
     );
 }
@@ -14,7 +23,8 @@ Movie.prototype = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 
 }
 
